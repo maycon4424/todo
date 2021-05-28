696c567a872d62e633ffa230c2c3e1494a03469e
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-28 18:20:36 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,95 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  tasks: any[] = [];
+
+  constructor(private alertControl: AlertController,
+              private toastControl: ToastController,
+              private actionSheetControl: ActionSheetController
+  ) {
+    let taskJSON = localStorage.getItem('taskDB');
+
+    if(taskJSON != null){
+      this.tasks = JSON.parse(taskJSON);
+    }
+  }
+
+  async showAdd(){
+    const alert = await this.alertControl.create({
+      header: 'O que deseja fazer?',
+      inputs: [
+        {name: 'task',
+        type: 'text',
+        placeholder: 'Digite a nova tarefa'
+        }
+      ],
+      buttons: [
+        {text: 'Cancelar',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Cancelou!')
+          }
+        },{
+          text: 'Adicionar',
+          handler: (form) => {
+            this.add(form.task);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async add(newTask: string){
+    if(newTask.trim().length < 1){
+      const toast = await this.toastControl.create({
+        message: 'Informe a tarefa!!',
+        duration: 2000,
+        position: 'top',
+        color: 'danger'
+      });
+      toast.present();
+      return;
+    }
+
+    let task = {name:newTask, done: false};
+    this.tasks.push(task);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage(){
+    localStorage.setItem('taskDB', JSON.stringify(this.tasks));
+  }
+
+  async openActions(task:any){
+    const actionSheet = await this.actionSheetControl.create({
+      header: 'O que fazer',
+      buttons: [{
+        text: task.done ? 'Desmarcar' : 'Marcar',
+        icon: task.done ? 'radio-button-off' : 'checkmark-circle',
+        handler: () => {
+          task.done = !task.done;
+          this.updateLocalStorage
+        },
+
+      },{
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelar selecionado!')
+        }
+      }
+    ]
+    });
+
+    await actionSheet.present();
+  }
+
+  deletar(task: any){
+    this.tasks = this.tasks.filter(taskArray => task != taskArray);
+    this.updateLocalStorage();
+  }
 
 }
